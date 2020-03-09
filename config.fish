66d4976059cde166cf57461527bd8cc42c49a755
Hampus Hauffman <Hampus.hauffman@gmail.com>
2020-03-10 00:41:34 +0100

    awd

--- config.fish
+++ config.fish
@@ -5,9 +5,3 @@ set -g theme_color_scheme dark
 alias v="nvim"
 alias p="sudo pacman -S"
 cat ~/.cache/wal/sequences &
-# Start X at login
-if status is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec startx -- -keeptty
-    end
-end
