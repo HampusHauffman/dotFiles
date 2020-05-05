4b7099bb5ab552f857a4d78a0ba52ab096005bd2
Hampus Hauffman <Hampus.hauffman@gmail.com>
2020-05-05 20:33:16 +0200

    dracula

--- .bashrc
+++ .bashrc
@@ -2,3 +2,4 @@ if [[ $(ps --no-header --pid=$PPID --format=cmd) != "fish" ]]
 then
 	exec fish
 fi
+
--- config.fish
+++ config.fish
@@ -3,5 +3,6 @@ end
 
 set -g theme_color_scheme dark
 alias v="nvim"
-alias p="sudo pacman -S"
+alias p="sudo pacman -Syu"
 cat ~/.cache/wal/sequences &
+
